bc3c963d6acf486623149a2a3e3f99c3ffe721f9
Matthew <metju312@gmail.com>
2021-12-06 18:19:12 +0100

    Dodanie konfiguracji firebase'a

---
+++ src/configs/firebase.config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3rkaC9_bI180TynPkaZGKHaFQ0WaZYco",
+  authDomain: "generic-store-a7cc4.firebaseapp.com",
+  projectId: "generic-store-a7cc4",
+  storageBucket: "generic-store-a7cc4.appspot.com",
+  messagingSenderId: "249509784061",
+  appId: "1:249509784061:web:25844e89582fd9f6e2d194",
+  measurementId: "G-QY41635N9Y"
+};
+
+const app = initializeApp(firebaseConfig);
